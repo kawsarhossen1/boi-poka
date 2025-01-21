869369f909f12c8fad1d6a7d13841f4bc54ef477
kawsarhossen1 <kawsarhossen66@gmail.com>
2025-01-21 23:53:08 +0600

    root file creat

---
+++ src/assets/Components/Root/Root.jsx
@@ -0,0 +1,11 @@
+
+
+const Root = () => {
+    return (
+        <div>
+            <h3>Hello</h3>
+        </div>
+    );
+};
+
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./assets/Components/Root/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
   },
 ]);
 
